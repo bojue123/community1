59cbb47237fa689e8898974a787d02537e4f6ed2
bojue123 <577651683@qq.com>
2019-11-12 07:57:45 +0800

    init repo

---
+++ src/main/java/life/ajie/community/controller/HelloController.java
@@ -0,0 +1,16 @@
+package life.ajie.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name")String name, Model model) {
+        model.addAttribute("name",name);
+        //字符串中的name对应html中的变量名为name，这里是把参数名为name的变量赋值给hello.html的name变量
+        return "hello";
+    }
+}
